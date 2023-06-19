4524b4332ae1c99a63f1eb32e7e6191d1cdce6ff
Komal <vanamala.lamok@gmail.com>
2023-06-19 09:42:13 -0700

    Initial Commit

---
+++ Menu App/Menu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_AppApp.swift
+//  Menu App
+//
+//  Created by Komal Vanamala on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Menu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
